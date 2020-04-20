e467b9458e987afb2b6f25b9d049de0fdaf236e8
heqi <qi.he@hand-china.com>
2020-04-20 11:17:04 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,11 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
-git push -f git@github.com:demoByHQ/demoByHQ.github.io.git master
+# https://github.com/demoByHQ/my-project
+# https://github.com/<USERNAME>/<REPO>
+# git push -f git@github.com:demoByHQ/demoByHQ.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:demoByHQ/my-project.git master:gh-pages
+git push -f git@github.com:demoByHQ/hq.git master
 
 cd -
